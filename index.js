54b42642c1c05042b127991d814ed198ccc26a9a
Karina Mogha <moghakarina@gmail.com>
2024-10-14 15:29:30 -0400

    yayy

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    return 2000/100
+}
+console.log(divide())
+
+const square = (number)=> {
+    return number*number
+}
+console.log(square)
+
+const add =(a,b)=> {
+    return a+b
+}
+
